3bbb0708a0ff78ad65b223458f16e45cffe4d8e4
arshahtsham <ahtshamarsh786@gmail.com>
2023-10-13 19:18:51 -0400

    Update sp500.py

--- sp500.py
+++ sp500.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
 import base64
+import altair as alt
 import yfinance as yf
 
 st.title('S&P 500 Stock Price Analysis')
 
 st.markdown("""
 This app retrieves the list of **S&P 500** companies and their corresponding **stock closing prices** for analysis.
-* **Python libraries:** pandas, streamlit, yfinance
+* **Python libraries:** base64, pandas, streamlit, numpy, altair, yfinance
 * **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
 """)
 
@@ -46,24 +47,19 @@ st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
 
 # Stock data retrieval and visualization
 st.sidebar.subheader('Stock Visualization Options')
-
-# Retrieve selected start and end dates
 start_date = st.sidebar.date_input('Start Date', pd.to_datetime('2023-01-01'))
 end_date = st.sidebar.date_input('End Date', pd.to_datetime('2023-12-31'))
 
-if start_date < end_date:
-    selected_symbol = st.sidebar.selectbox('Select a Company', list(df_selected_sector['Symbol']))
-    company_data = yf.download(selected_symbol, start=start_date, end=end_date)
+selected_symbol = st.sidebar.selectbox('Select a Company', list(df_selected_sector['Symbol']))
+company_data = yf.download(selected_symbol, start=start_date, end=end_date)
 
-    st.header(f'Stock Closing Price for {selected_symbol}')
-    st.write(f'Data for {selected_symbol} from {start_date} to {end_date}')
-    st.line_chart(company_data['Close'])
+st.header('Stock Closing Price for ' + selected_symbol)
+st.write(f'Data for {selected_symbol} from {start_date} to {end_date}')
+st.line_chart(company_data['Close'])
 
-    # Show a table with detailed stock data
-    st.subheader('Stock Data')
-    st.write(company_data)
-else:
-    st.error("Please select a valid date range (Start Date should be before End Date).")
+# Show a table with detailed stock data
+st.subheader('Stock Data')
+st.write(company_data)
 
 # Provide insights and explanations
 st.markdown("""
@@ -73,12 +69,3 @@ st.markdown("""
 - Analyze the stock's performance during the selected period.
 """)
 
-# Deployable from GitHub
-st.markdown("### Deployment")
-st.markdown("This app is deployable from GitHub.")
-st.markdown("GitHub Repository: [Your GitHub Repository Link](https://github.com/yourusername/your-repo)")
-
-# Share the Streamlit link
-st.markdown("### Streamlit Link")
-st.markdown("You can access this app using the following Streamlit Share link:")
-st.markdown("Streamlit Share Link: [Your Streamlit Share Link](https://share.streamlit.io/yourusername/your-repo/app.py)")
